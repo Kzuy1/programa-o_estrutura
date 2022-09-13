071d57b41da5ec3c1a5847f24643af5ce57f02f6
Kzuy1 <leoczuy@gmail.com>
2022-09-12 21:57:48 -0300

    Add files via upload

--- aula 09_vetores02/ex2.3.cpp
+++ aula 09_vetores02/ex2.3.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <locale.h>
+#include <cctype>
+
 
 //Solicitar um nome completo e armazenar num vetor.
 //Então, mostre o nome em caixa alta(letras maiúsculas)
@@ -12,6 +14,18 @@ int main() {
 	srand( time(NULL));
 	int i;
 	char a[30];
+	
+	for (i = 0; i < 30; i ++){
+		a[i] = 0;
+	}
+	
+	printf("\nDigite seu nome completo:\n");
+	gets(a);
+	
+	for (i = 0; i < 30; i++){
+		printf("%c", toupper(a[i]));
+	}
+	
 	
 	
 }
