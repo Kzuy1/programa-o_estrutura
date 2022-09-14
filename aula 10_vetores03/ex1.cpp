6ce0cef6d8e2d5d1e67961f07f0ccfa02c759680
Kzuy1 <leoczuy@gmail.com>
2022-09-13 21:30:11 -0300

    Add files via upload

--- aula 10_vetores03/ex1.cpp
+++ aula 10_vetores03/ex1.cpp
@@ -11,7 +11,23 @@ int main(){
 	
 	int a[10],b[10],c[10];
 	
-	for ()
+	for (i = 0; i < 10; i++){
+		a[i] = rand()%20;
+		b[i] = rand()%10;
+		c[i] = a[i] + b[i];
+	}
+	printf("\nVetor A:\n");
+	for (i = 0; i < 10; i++){
+		printf("%d ", a[i]);
+	}
+	printf("\nVetor B:\n");
+	for (i = 0; i < 10; i++){
+		printf("%d ", b[i]);
+	}
+	printf("\nVetor A + B:\n");
+	for (i = 0; i < 10; i++){
+		printf("%d ", c[i]);
+	}
 	
 
 }
