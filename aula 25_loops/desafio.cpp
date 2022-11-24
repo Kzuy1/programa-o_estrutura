57b1d9b8c62f57fd710cf52cb42e38bba72e7084
Kzuy1 <leoczuy@gmail.com>
2022-11-24 20:00:41 -0300

    Add files via upload
    Signed-off-by: Kzuy1 <leoczuy@gmail.com>

--- aula 25_loops/desafio.cpp
+++ aula 25_loops/desafio.cpp
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <locale.h>
+#include<iostream>
+using namespace std;
 
 int matriz[20][20];
 
@@ -19,16 +22,83 @@ void le_arquivo(){
 	}
 }
 
+int multVertical(){
+	int lin, col, aux, cal;
+	int posLin, posCol;
+	aux = 0;
+	for(col = 0; col < 20; col++){
+		for(lin = 0; lin <17; lin++){
+			cal = matriz[lin][col] * matriz[lin+1][col] * matriz[lin+2][col] * matriz[lin+3][col];
+			if(cal > aux){
+				aux = cal;
+				posLin = lin;
+				posCol = col;
+			}
+		}
+	}
+	printf("Número: %d\tPosição Linha: %d\tPosição Coluna %d\n", aux, posLin, posCol);
+}
+
+int multHorizontal(){
+	int lin, col, aux, cal;
+	int posLin, posCol;
+	aux = 0;
+	for(lin = 0; lin < 20; lin++){
+		for(col = 0; col < 17; col++){
+			cal = matriz[lin][col] * matriz[lin][col+1] * matriz[lin][col+2] * matriz[lin][col+3];
+			if(cal > aux){
+				aux = cal;
+				posLin = lin;
+				posCol = col;
+			}
+		}
+	}
+	printf("Número: %d\tPosição Linha: %d\tPosição Coluna %d\n", aux, posLin, posCol);
+}
+
+int multDiagonal45(){
+	int lin, col, aux, cal;
+	int posLin, posCol;
+	aux = 0;
+	for(lin = 0; lin < 17; lin++){
+		for(col = 3; col < 20; col++){
+			cal = matriz[lin][col] * matriz[lin+1][col-1] * matriz[lin+2][col-2] * matriz[lin+3][col-3];
+			if(cal > aux){
+				aux = cal;
+				posLin = lin;
+				posCol = col;
+			}
+		}
+	}
+	printf("Número: %d\tPosição Linha: %d\tPosição Coluna %d\n", aux, posLin, posCol);
+}
+
+int multDiagonal135(){
+	int lin, col, aux, cal;
+	int posLin, posCol;
+	aux = 0;
+	for(lin = 0; lin < 1; lin++){
+		for(col = 0; col < 17; col++){
+			cal = matriz[lin][col] * matriz[lin+1][col+1] * matriz[lin+2][col+2] * matriz[lin+3][col+3];
+			if(cal > aux){
+				aux = cal;
+				posLin = lin;
+				posCol = col;
+			}
+		}
+	}
+	printf("Número: %d\tPosição Linha: %d\tPosição Coluna %d\n", aux, posLin, posCol);
+}
 
 
 int main(){
+	setlocale(LC_ALL, "Portuguese");
 	le_arquivo();
-	
-	for(int lin = 0; i < 20; i++){
-		for(int col = 0; c <20; c++){
-			printf("%d ", matriz[lin][col]);
-		}
-		printf("\n");
-	}
+	int lin, col, aux, cal;
+	lin = 6, col = 15;
+	multVertical();
+	multHorizontal();
+	multDiagonal45();
+	multDiagonal135();
 }
 
